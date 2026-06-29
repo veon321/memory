e804339088c9310c59567ba8ce9f9228442e1365
unknown <veon@vp.pl>
2026-06-29 15:54:07 +0200

    memory

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttonAddElement = document.getElementById("addElement");
 const container = document.getElementById("container");
+const timeRound = document.getElementById("time");
+const score = document.getElementById("score");
 
 let cardsInGame = [];
 
@@ -45,30 +47,63 @@ function createCards() {
   });
 }
 
-let klik = 0;
-let klik1 = "nic";
+let click = 0;
+let click1 = "";
+let timeToEndRound = 5;
+let timeToEndRoundInterval = null;
+let isLocked = false;
+let currentScore = 0;
 
 container.addEventListener("click", (event) => {
+  if (isLocked) return;
   const clickedCard = event.target.closest(".card-container");
   const card = clickedCard.querySelector("img");
+
+  if (card.classList.contains("revealed")) {
+    return;
+  }
+
+  if (timeToEndRoundInterval) clearInterval(timeToEndRoundInterval);
+
+  timeToEndRoundInterval = setInterval(() => {
+    if (timeToEndRound <= 1) {
+      clearInterval(timeToEndRoundInterval);
+      click = 0;
+      card.classList.remove("revealed");
+      timeToEndRound = 5 + 1;
+    }
+    timeToEndRound -= 1;
+    timeRound.innerHTML = `${timeToEndRound}s`;
+  }, 1000);
 
   console.log(card);
 
   if (!card) return;
   card.classList.add("revealed");
-  if (klik == 0) {
-    klik1 = card;
-    klik = 1;
-  } else if (klik === 1) {
-    if (card.alt === klik1.alt) {
-      console.log("para");
-      klik = 0;
+  if (click == 0) {
+    timeToEndRound = 5;
+    click1 = card;
+    click = 1;
+  } else if (click === 1) {
+    // para
+    if (card.alt === click1.alt) {
+      click = 0;
+      clearInterval(timeToEndRoundInterval);
+      currentScore += 5 + timeToEndRound;
+      score.innerHTML = `Score: ${currentScore}`;
+      timeToEndRound = 5;
+      timeRound.innerHTML = `${timeToEndRound}s`;
     } else {
-      console.log("Pudło!");
+      // pudlo
+      isLocked = true;
+      clearInterval(timeToEndRoundInterval);
+      timeToEndRound = 5;
+      timeRound.innerHTML = `${timeToEndRound}s`;
       setTimeout(() => {
         card.classList.remove("revealed");
-        klik1.classList.remove("revealed");
-        klik = 0;
+        click1.classList.remove("revealed");
+        click = 0;
+        isLocked = false;
       }, 1500);
     }
   }
